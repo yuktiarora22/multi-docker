c5bce0ca8e615e75238ba4e5d39034e38726aaff
yuktiarora22 <yukti22@gmail.com>
2024-12-19 02:09:15 +0530

    changed other page

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="App-header">
           <h1 className="App-title">Fib calculator</h1>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other Blank Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib}></Route>
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default () => {
   return (
     <div>
-      I'm some other page!
+      I'm some other blank page! I don't have much to offer.
       <Link to="/">Go back home</Link>
     </div>
   );
